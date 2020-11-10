6ba7802c4de70cd885368213785806a73c74cb22
Mohammadreza <mohammad.r.ashouri@gmail.com>
2020-11-10 20:37:32 +0100

    ...

---
+++ Models/Squad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCsharpSearch.Models
+{
+    public class Squad
+    {
+        public float searchscore { get; set; }
+
+        public string paragraph_index { get; set; }
+
+        public string paragraph_text { get; set; }
+
+        public string keyphrases { get; set; }
+
+        // public string[] keyphrases { get; set; }
+    }
+}
